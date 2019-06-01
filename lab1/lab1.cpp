793bb4884162fc61e5cf8ba3ada1100b171640aa
Mengjing Xu <annie_anna@sjtu.edu.cn>
2019-05-31 17:35:08 -0700

    lab1 finished

---
+++ lab1/lab1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "lab1.h"
+
+int main() {
+    test_magnanimous();
+    test_jordan_polya();
+    test_alternating();
+    test_duffinian();
+    test_apocalyptic();
+    return 0;
+}
